c2e0015d612a139bf1d7fbfe7ef42190e0019fae
sylwiabr <s.brodacka@gmail.com>
2018-06-05 06:44:48 +0200

    dropRow in C

--- native_libs/src/parse_csv.c
+++ native_libs/src/parse_csv.c
@@ -204,6 +204,42 @@ char **copy_rows(char **mat, size_t rows, size_t cols, size_t stride_r, size_t s
     return new_mat;
 }
 
+char **drop_row(char **mat, size_t rows, size_t cols, size_t stride_r, size_t stride_c, int row_to_drop)
+{
+    if (row_to_drop>rows)
+        return NULL;
+
+    if (mat == NULL)
+        return NULL;
+
+    int rows_to_copy[rows-1];
+        for(int i = 0; i < row_to_drop; i++) {
+                rows_to_copy[i] = i;
+        }
+        for(int i = row_to_drop+1; i <= rows; i++) {
+                rows_to_copy[i-1] = i;
+        }
+
+    char **new_mat = malloc((rows-1)*cols*sizeof(*mat));
+
+    for(size_t r = 0; r < rows-1; r++)
+    {
+        for(size_t c = 0; c < cols; c++)
+        {
+            int row =  rows_to_copy[r];
+            int elem = (row * stride_r + c * stride_c);
+            // printf("%s: %d\n","elem",elem );
+            int new_index = r * stride_r + c * stride_c;
+            // printf("%s: %d\n","new_index",new_index);
+            char *tmp = mat[elem];
+            // printf("%s\n", tmp ? tmp : "NULL");
+            memcpy(&new_mat[new_index], &mat[elem], sizeof(*mat));
+
+        }
+    }
+    return new_mat;
+}
+
 char **transpose(char **mat, size_t rows, size_t cols, size_t stride_r, size_t stride_c)
 {
     if (mat == NULL)
