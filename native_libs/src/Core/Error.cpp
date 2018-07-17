cf9f35d53823a8e28b2df56428074da5028528df
mwu <mwu-tow@gazeta.pl>
2018-07-17 20:42:48 +0200

    Don't crash if caller passes nullptr as outError.

--- native_libs/src/Core/Error.cpp
+++ native_libs/src/Core/Error.cpp
@@ -1,11 +1,27 @@
 #include "Error.h"
 
+#include <iostream>
 #include <utility>
 
 namespace
 {
 	const auto unknownInternalErrorText = "Unknown internal error encountered";
 	thread_local std::string errorMessage;
+}
+
+void writeError(const char **outError, const char *toWrite)
+{
+	if(outError)
+	{
+		*outError = toWrite;
+	}
+	else if(toWrite)
+	{
+		// should not happen unless caller gives nullptr as error target
+		// in such case we will just write the error to stdout
+		// as we have no other means of getting user's attention
+		std::cout << "outError==nullptr, failed to set error message: " << toWrite << std::endl;
+	}
 }
 
 void setError(const char **outError, const char *errorToSet, const char *functionName) noexcept
@@ -17,17 +33,17 @@ void setError(const char **outError, const char *errorToSet, const char *functio
 		else
 			errorMessage = errorToSet;
 
-		*outError = errorMessage.c_str();
+		writeError(outError, errorMessage.c_str());
 	}
 	catch(...)
 	{
 		// should happen practically never, perhaps if error string is too long to fit in memory....
 		// but to be on the safe side
-		*outError = unknownInternalErrorText;
+		writeError(outError, unknownInternalErrorText);
 	}
 }
 
 void clearError(const char **outError) noexcept
 {
-	*outError = nullptr;
+	writeError(outError, nullptr);
 }
