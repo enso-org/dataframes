9a96bb1f44188a993bb7f9ece867371ce5007fce
sylwiabr <s.brodacka@gmail.com>
2018-07-25 11:11:21 +0200

    choose default point color

--- visualizers/plotly/plotly.js
+++ visualizers/plotly/plotly.js
@@ -21,15 +21,19 @@
         r = header.indexOf('color.r')
         g = header.indexOf('color.g')
         b = header.indexOf('color.b')
+        a = header.indexOf('color.a')
         s = header.indexOf('size')
         labels = header.indexOf('labels')
 
-        color = dataSeries.map(function(p){ return ('rgb(' + p[r] + ',' + p[g] + ',' + p[b] + ')')});
-        datax = dataSeries.map(function(p) { return p[x]; });
-        datay = dataSeries.map(function(p) { return p[y]; });
-        dataz = dataSeries.map(function(p) { return p[z]; });
+        color = dataSeries.map(function(p){
+            return p[r] !== " " && p[g] !== " " && p[b] !== " " && p[a] !== " " ? 'rgb(' + p[r] + ',' + p[g] + ',' + p[b] + ',' + p[a] + ')'  : 'rgb(' + 253 + ',' + 106 + ',' + 2 + ',' + 1 + ')' 
+        });
+        datax = dataSeries.map(function(p) { return p[x] !== " " ? p[x] : 0; });
+        datay = dataSeries.map(function(p) { return p[y] !== " " ? p[y] : 0; });
+        dataz = dataSeries.map(function(p) { return p[z] !== " " ? p[z] : 0; });
         sizes = dataSeries.map(function(p) { return p[s]; });
         dataLabels = dataSeries.map(function(p) { return p[labels]; });
+        console.log(datay)
         var trace = {
           x: datax,
           y: datay,
