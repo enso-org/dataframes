2fe2e1e9db9c8bae413c33ae6fc5e6f3bf228d93
mwu <mwu-tow@gazeta.pl>
2018-09-22 20:22:28 +0200

    Hardcoded RSI function.

--- native_libs/src/Analysis.cpp
+++ native_libs/src/Analysis.cpp
@@ -103,12 +103,12 @@ template<typename T>
 struct Mean
 {
     int64_t count = 0;
-    double accumulator = 0;
+    T accumulator = 0;
     static constexpr const char *name = "mean";
     static constexpr int32_t RequiredSampleCount = 1;
     void operator() (T elem) { accumulator += elem; count++; }
     void operator() () {}
-    auto get() { return accumulator / count; }
+    auto get() { return accumulator / (double)count; }
 };
 
 template<typename T>
@@ -204,6 +204,31 @@ struct Last
 
 };
 
+struct RSI
+{
+    static constexpr const char *name = "RSI";
+    static constexpr int32_t RequiredSampleCount = 1;
+
+    Mean<double> down, up;
+
+    void operator()(double value)
+    {
+        if(value > 0)
+            up(value);
+        else
+            down(value);
+    }
+    void operator() () {}
+
+    double get()
+    {
+        const auto upMean = up.get();
+        const auto downMean = down.get();
+        return 100 * upMean / (upMean - downMean);
+    }
+
+};
+
 template<AggregateFunction aggr, typename T>
 struct AggregatorFor {};
 
@@ -215,6 +240,7 @@ template<typename T> struct AggregatorFor<AggregateFunction::Median , T> { using
 template<typename T> struct AggregatorFor<AggregateFunction::First  , T> { using type = First<T>  ; };
 template<typename T> struct AggregatorFor<AggregateFunction::Last   , T> { using type = Last<T>   ; };
 template<typename T> struct AggregatorFor<AggregateFunction::Sum    , T> { using type = Sum<T>    ; };
+template<typename T> struct AggregatorFor<AggregateFunction::RSI    , T> { using type = RSI       ; };
 
 template<arrow::Type::type id, typename Processor>
 auto calculateStatScalar(const arrow::Column &column, Processor &p)
--- native_libs/src/Analysis.h
+++ native_libs/src/Analysis.h
@@ -65,7 +65,7 @@ struct GroupedKeyInfo
 
 enum class AggregateFunction : int8_t
 {
-    Minimum, Maximum, Mean, Length, Median, First, Last, Sum
+    Minimum, Maximum, Mean, Length, Median, First, Last, Sum, RSI
 };
 
 template<typename Function>
@@ -81,6 +81,7 @@ auto dispatchAggregateByEnum(AggregateFunction aggregateEnum, Function &&f)
     CASE_DISPATCH(AggregateFunction::First)
     CASE_DISPATCH(AggregateFunction::Last)
     CASE_DISPATCH(AggregateFunction::Sum)
+    CASE_DISPATCH(AggregateFunction::RSI)
     default: throw std::runtime_error("not supported aggregate function " + std::to_string((int)aggregateEnum));
     }
 }
