333b2e94233ec34b9538ff296bddeff9f91d5fc9
mwu <mwu-tow@gazeta.pl>
2018-10-05 00:19:24 +0200

    this should make GCC to eat trailing coma upon expansion of empty variadic pack in macro

--- native_libs/src/Core/Common.h
+++ native_libs/src/Core/Common.h
@@ -165,9 +165,9 @@ std::vector<T> iotaVector(size_t N, T from = T{})
 
 
 #define THROW(message, ...)  do {                                         \
-	auto msg_ =  fmt::format(message, __VA_ARGS__);                       \
+	auto msg_ =  fmt::format(message, ##__VA_ARGS__);                     \
 	std::cerr << __FILE__ << " " << __LINE__ << " " << msg_ << std::endl; \
-	std::runtime_error e_to_throw{fmt::format(message, __VA_ARGS__)};     \
+	std::runtime_error e_to_throw{fmt::format(message, ##__VA_ARGS__)};   \
 	throw e_to_throw;                                                     \
 } while(0)
 
