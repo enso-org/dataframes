10c8cb15c1b88027602927df384a63e2d823b256
mwu <mwu-tow@gazeta.pl>
2018-11-07 13:29:07 +0100

    one more using…

--- native_libs/src/variant.h
+++ native_libs/src/variant.h
@@ -7,6 +7,7 @@ using std::visit;
 using std::get;
 using std::get_if;
 using std::holds_alternative;
+using std::variant_alternative_t;
 
 #else  // Mac OS
 
@@ -16,5 +17,6 @@ using mpark::visit;
 using mpark::get;
 using mpark::get_if;
 using mpark::holds_alternative;
+using mpark::variant_alternative_t;
 
 #endif
