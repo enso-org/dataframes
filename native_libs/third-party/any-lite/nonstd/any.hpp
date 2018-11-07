ff877084d9489f85ad13012d06c8f60fa04bb78d
mwu <mwu-tow@gazeta.pl>
2018-11-07 19:38:52 +0100

    don't use C++17 std::any on macOS

--- native_libs/third-party/any-lite/nonstd/any.hpp
+++ native_libs/third-party/any-lite/nonstd/any.hpp
@@ -49,7 +49,7 @@
 
 // use C++17 std::any if available and requested:
 
-#if any_CPP17_OR_GREATER && defined(__has_include ) && __has_include( <any> )
+#if any_CPP17_OR_GREATER && defined(__has_include ) && __has_include( <any> )  && !defined(__APPLE__)
 # define any_HAVE_STD_ANY  1
 #else
 # define any_HAVE_STD_ANY  0
