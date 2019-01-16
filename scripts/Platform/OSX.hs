eeed1d4dbeb73718db87deca1b3db9a7a45955d8
mwu <mwu-tow@gazeta.pl>
2019-01-16 02:09:55 +0100

    don't set chmod 555 before processing binaries is done

--- scripts/Platform/OSX.hs
+++ scripts/Platform/OSX.hs
@@ -93,7 +93,6 @@ installBinary targetBinariesDir sourcePath = do
             -- local dependencies of local dependencies are in the same folder as the current binary
             -- NOTE: in future, in multi-package world, there might be more folders
             INT.change destinationPath installName $ "@loader_path" </> takeFileName installName
-    callProcess "chmod" ["555", destinationPath]
     pure destinationPath
 
 -- If installed libraries list contains library with the same name (until dot)
