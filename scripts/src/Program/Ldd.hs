7031d5613fb95a2945b3c73226cbcec3d5380c02
Michał Wawrzyniec Urbańczyk <mwu-tow@gazeta.pl>
2019-03-14 17:30:06 +0100

    Build scripts refactoring part 1 (#136)
    * reorganized code into lib+exe
    
    * Replaced IO with MonadIO.
    
    * Migrating towards typed-process
    
    * Program now uses typed-process. Cleanups. Added haddock strings.
    
    * further typed-process migration. Documentation.
    
    * split out the make program
    
    * Update scripts/package.yaml
    
    Co-Authored-By: mwu-tow <mwu-tow@gazeta.pl>
    
    * process is not used anymore
    
    * explicit imoprt list
    
    * rename OSX -> MacOS
    
    * use stackage lts-13.12
    
    * wrapped comments

--- scripts/src/Program/Ldd.hs
+++ scripts/src/Program/Ldd.hs
@@ -2,13 +2,12 @@ module Program.Ldd where
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Maybe
 import Data.Monoid
--- import Data.Text.Encoding (decodeUtf8)
 import Data.String.Utils
 import System.FilePath
 import System.Directory
-import System.Process
 import Text.Printf
 
 import Program
@@ -57,7 +56,7 @@ parseNamePathAddress (TL.strip -> input) =
 -- Returns list of shared libraries that are necessary to load given binary
 -- executable image.
 -- Note: this are not necessarily all the dependencies.
-dependenciesOfBinary :: FilePath -> IO [FilePath]
+dependenciesOfBinary :: (MonadIO m) => FilePath -> m [FilePath]
 dependenciesOfBinary binary = do
     lddOutput <- TL.pack <$> readProgram @Ldd [binary]
     let parsedLibraryInfo = parseNamePathAddress <$> TL.lines lddOutput
@@ -66,9 +65,9 @@ dependenciesOfBinary binary = do
 -- Returns list of shared libraries that are necessary to load given binary
 -- executable images.
 -- Note: this are not necessarily all the dependencies.
-dependenciesOfBinaries :: [FilePath] -> IO [FilePath]
+dependenciesOfBinaries :: (MonadIO m) => [FilePath] -> m [FilePath]
 dependenciesOfBinaries binaries = do
     listOfListOfDeps <- mapM dependenciesOfBinary binaries
     -- get rid of duplicates by passing through set
     let listOfDeps = Set.toList $ Set.unions $ Set.fromList <$> listOfListOfDeps
-    filterM doesFileExist listOfDeps
+    filterM (liftIO <$> doesFileExist) listOfDeps
--- scripts/src/Program/Patchelf.hs
+++ scripts/src/Program/Patchelf.hs
@@ -1,5 +1,6 @@
 module Program.Patchelf where
 
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.List
 import Data.String.Utils
 import System.FilePath
@@ -19,22 +20,22 @@ relativeRpath binaryPath dependenciesDir = "$ORIGIN" </> relativeNormalisedPath 
 
 -- Given a path to binary image, sets rpath on the binary so the directories
 -- given as second argument are added as paths relative to the binary.
-setRelativeRpath :: FilePath -> [FilePath] -> IO ()
+setRelativeRpath :: (MonadIO m) => FilePath -> [FilePath] -> m ()
 setRelativeRpath binaryPath depsDirectories = setRpath binaryPath $ relativeRpath binaryPath <$> depsDirectories
 
 -- returns runpath if present, or secondarily rpath if present, or fails if neither present
-getRpath :: FilePath -> IO String
+getRpath :: (MonadIO m) => FilePath -> m String
 getRpath binaryPath =
     strip <$> readProgram @Patchelf ["--print-rpath", binaryPath]
 
 -- sets runpath on executable (overwriting if present)
-setRpath :: FilePath -> [String] -> IO ()
+setRpath :: (MonadIO m) => FilePath -> [String] -> m ()
 setRpath binaryPath rpath = do
     let rpathFormatted = intercalate ":" rpath
-    putStrLn $ printf "Setting rpath on %s to %s" binaryPath rpathFormatted
+    liftIO $ putStrLn $ printf "Setting rpath on %s to %s" binaryPath rpathFormatted
     call @Patchelf ["--set-rpath", rpathFormatted, "--force-rpath", binaryPath]
 
 -- NOTE: requires relatively new version of patchelf (likely 0.9), otherwise fail with stupid message "stat: No such file or directory"
-removeRpath :: FilePath -> IO ()
+removeRpath :: (MonadIO m) => FilePath -> m ()
 removeRpath binaryPath = call @Patchelf ["--remove-rpath", binaryPath]
 
