e12b881d7d4f4f77f841841ef9a882889188837d
sylwiabr <s.brodacka@gmail.com>
2019-06-19 17:25:36 +0200

    add folders with samples and data to package

--- scripts/Main.hs
+++ scripts/Main.hs
@@ -176,7 +176,7 @@ package repoDir stagingDir buildArtifacts = do
     let packageRoot = stagingDir </> "Dataframes"
     let packageBinariesDir = packageRoot </> "native_libs" </> nativeLibsOsDir
 
-    let dirsToCopy = ["src", "visualizers", ".luna-package"]
+    let dirsToCopy = ["src", "visualizers", ".luna-package", "samples", "data"]
     mapM (copyDirectory repoDir packageRoot) dirsToCopy
     copyToDir packageRoot $ repoDir </> "snippets.yaml"
 
